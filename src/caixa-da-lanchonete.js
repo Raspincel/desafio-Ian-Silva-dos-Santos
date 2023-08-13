ee5284c39a7abd3ae086ba07da33bafd8eabb528
Raspincel <raspincel@gmail.com>
2023-08-13 17:20:41 -0300

    desafio completo

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,9 +1,102 @@
+import items from './items.js'
+
 class CaixaDaLanchonete {
 
-    calcularValorDaCompra(metodoDePagamento, itens) {
-        return "";
+    pedido = []
+    modificadorDoPreco = 1
+
+    pegarPedido(itens) {
+
+        this.pedido = itens.map(item => {
+            
+            const pedido = item.split(',')
+            const codigo = pedido[0]
+            const quantidade = Number(pedido[1])
+            
+            const preco = (()=> {
+                for (const item of items) {
+                    if (item.code === codigo)
+                        return item.price
+                }
+            })()
+
+            return {
+                codigo: codigo,
+                quantidade: quantidade,
+                preco: preco
+            }
+        });
     }
 
+    possuiExtrasValidos() {
+        const verificacao = (principal, extra)=> {
+            return this.pedido.some(item => item.codigo === extra) && !this.pedido.some(item => item.codigo === principal)
+        }
+
+        if (verificacao('cafe', 'chantily'))
+            return false
+
+        if (verificacao('sanduiche', 'queijo'))
+            return false
+
+        return true
+    }
+
+    pediuQuantidadeValida() {
+        return !this.pedido.some(item => { 
+            return item.quantidade === 0
+        })
+    }
+
+    inseriuCodigoValido() {
+        const codigos = new Set()
+        items.forEach(item => codigos.add(item.code))
+
+        return this.pedido.every(item => codigos.has(item.codigo))
+    }
+
+    inseriuFormaDePagamentoValida(metodoDePagamento) {
+        const formas = ["dinheiro", "debito", "credito"]
+        return formas.includes(metodoDePagamento)
+    }
+
+    pegarModificadorDoPreco(metodoDePagamento) {
+        if (metodoDePagamento === "dinheiro")
+            this.modificadorDoPreco = 0.95
+        
+        if (metodoDePagamento === "credito")
+            this.modificadorDoPreco = 1.03
+    }
+
+    calcularValorDaCompra(metodoDePagamento, itens) {
+        
+        if (itens.length === 0)
+            return "Não há itens no carrinho de compra!"
+        
+        this.pegarPedido(itens)
+        
+        if (!this.pediuQuantidadeValida())
+            return "Quantidade inválida!"
+
+        if (!this.inseriuCodigoValido()) 
+            return "Item inválido!"
+
+        if (!this.possuiExtrasValidos())
+            return "Item extra não pode ser pedido sem o principal"
+
+        if (!this.inseriuFormaDePagamentoValida(metodoDePagamento))
+            return "Forma de pagamento inválida!"
+
+        this.pegarModificadorDoPreco(metodoDePagamento)
+
+        const valorBase = this.pedido.reduce((acc, item)=> {
+            return acc + (item.preco * item.quantidade)
+        }, 0)
+
+        const valor = (valorBase * this.modificadorDoPreco).toFixed(2).replace('.', ',')
+
+        return `R$ ${valor}`;
+    }
 }
 
 export { CaixaDaLanchonete };
---
+++ src/items.js
@@ -0,0 +1,42 @@
+export default [
+    {
+        code: 'cafe',
+        description: 'Café',
+        price: 3.00
+    },
+    {
+        code: 'chantily',
+        description: 'Chantily (extra do Café)',
+        price: 1.50
+    },
+    {
+        code: 'suco',
+        description: 'Suco Natural',
+        price: 6.20
+    },
+    {
+        code: 'sanduiche',
+        description: 'Sanduíche',
+        price: 6.50
+    },
+    {
+        code: 'queijo',
+        description: 'Queijo (extra do Sanduíche)',
+        price: 2.00
+    },
+    {
+        code: 'salgado',
+        description: 'Salgado',
+        price: 7.25
+    },
+    {
+        code: 'combo1',
+        description: '1 Suco e 1 Sanduíche',
+        price: 9.50
+    },
+    {
+        code: 'combo2',
+        description: '1 Café e 1 Sanduíche',
+        price: 7.50
+    }
+];
